ed986bbe4ae9bc5eedcabd69fd576398742308fc
SamirAhma <30921483+SamirAhma@users.noreply.github.com>
2022-11-25 14:30:21 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
 import Login from "./pages/login/Login";
+import Test from "./pages/test/test";
 
 function App() {
   const admin = JSON.parse(
@@ -19,6 +20,9 @@ function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/test">
+          <Test />
+        </Route>
         <Route path="/login">
           <Login />
         </Route>
